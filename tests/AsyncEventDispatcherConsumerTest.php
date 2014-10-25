e40fe74b1212f868bee24e8b00206f3432abb0fc
Miquel Company Rodriguez <miquel@solilokiam.com>
2014-10-25 21:05:24 +0200

    Improve AsyncEventDispatcherConsumer test coverage.

--- tests/AsyncEventDispatcherConsumerTest.php
+++ tests/AsyncEventDispatcherConsumerTest.php
@@ -13,11 +13,6 @@ use Solilokiam\AsyncEventDispatcher\AsyncEventDispatcherConsumer;
 
 class AsyncEventDispatcherConsumerTest extends \PHPUnit_Framework_TestCase
 {
-    protected function tearDown()
-    {
-        m::close();
-    }
-
     public function testActivateConsume()
     {
         $eventDriverMock = m::mock('Solilokiam\AsyncEventDispatcher\EventDriver\EventDriverInterface');
@@ -25,7 +20,8 @@ class AsyncEventDispatcherConsumerTest extends \PHPUnit_Framework_TestCase
 
         $eventListenerManagerMock = m::mock('Solilokiam\AsyncEventDispatcher\AsyncEventDispatcherListenerManager');
 
-        $asyncEventDispatcherConsumer = new AsyncEventDispatcherConsumer($eventDriverMock, $eventListenerManagerMock, 'foo.event');
+        $asyncEventDispatcherConsumer = new AsyncEventDispatcherConsumer($eventDriverMock, $eventListenerManagerMock,
+            'foo.event');
         $asyncEventDispatcherConsumer->activateConsumer(10);
     }
 
@@ -39,6 +35,66 @@ class AsyncEventDispatcherConsumerTest extends \PHPUnit_Framework_TestCase
         $asyncEventDispatcherConsumer = new AsyncEventDispatcherConsumer($eventDriverMock, $eventListenerManagerMock, 'foo.event');
 
         $asyncEventDispatcherConsumer->consume($asyncEventMock);
+    }
+
+    public function testConsumeWithListeners()
+    {
+        $eventDriverMock = m::mock('Solilokiam\AsyncEventDispatcher\EventDriver\EventDriverInterface');
+        $eventListenerManagerMock = m::mock('Solilokiam\AsyncEventDispatcher\AsyncEventDispatcherListenerManager');
+
+        $listener1 = function () use (&$invoked) {
+            $invoked[] = '1';
+        };
+
+        $listener2 = function () use (&$invoked) {
+            $invoked[] = '2';
+        };
+
+        $eventListenerManagerMock->shouldReceive('getListeners')->times(1)->with('foo.event')->andReturn(array(
+            $listener1,
+            $listener2
+        ));
+        $asyncEventMock = m::mock('Solilokiam\AsyncEventDispatcher\AsyncEvent');
+        $asyncEventMock->shouldReceive('isPropagationStopped')->times(2)->andReturn(false);
+
+        $asyncEventDispatcherConsumer = new AsyncEventDispatcherConsumer($eventDriverMock, $eventListenerManagerMock, 'foo.event');
+
+        $asyncEventDispatcherConsumer->consume($asyncEventMock);
+
+        $this->assertEquals(array('1', '2'), $invoked);
+    }
+
+    public function testConsumeWithListenersPropagationStopped()
+    {
+        $eventDriverMock = m::mock('Solilokiam\AsyncEventDispatcher\EventDriver\EventDriverInterface');
+        $eventListenerManagerMock = m::mock('Solilokiam\AsyncEventDispatcher\AsyncEventDispatcherListenerManager');
+
+        $listener1 = function () use (&$invoked) {
+            $invoked[] = '1';
+        };
+
+        $listener2 = function () use (&$invoked) {
+            $invoked[] = '2';
+        };
+
+        $eventListenerManagerMock->shouldReceive('getListeners')->times(1)->with('foo.event')->andReturn(array(
+            $listener1,
+            $listener2
+        ));
+        $asyncEventMock = m::mock('Solilokiam\AsyncEventDispatcher\AsyncEvent');
+        $asyncEventMock->shouldReceive('isPropagationStopped')->times(1)->andReturn(true);
+
+        $asyncEventDispatcherConsumer = new AsyncEventDispatcherConsumer($eventDriverMock, $eventListenerManagerMock,
+            'foo.event');
+
+        $asyncEventDispatcherConsumer->consume($asyncEventMock);
+
+        $this->assertEquals(array('1'), $invoked);
+    }
+
+    protected function tearDown()
+    {
+        m::close();
     }
 
 }
